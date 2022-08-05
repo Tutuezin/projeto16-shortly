d91bc849f511575397a603cffdc5ae19e89415ea
Tutuezin <vinnycamara1@gmail.com>
2022-08-05 16:25:02 -0300

    feat: POST /urls/shorten > OK!

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -1,9 +1,19 @@
 import connection from "../database/pgsql.js";
+import { nanoid } from "nanoid";
 
 export async function createShortenUrl(req, res) {
   const { user } = res.locals;
+  const { url } = req.body;
+
+  const shortUrl = nanoid();
+  console.log(user.id);
+
   try {
-    res.status(201).send("foi");
+    const shortenUrl = await connection.query(
+      `INSERT INTO links ("shortUrl", url, "userId") VALUES ($1, $2, $3)`,
+      [shortUrl, url, user.id]
+    );
+    res.status(201).send({ shortUrl });
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
